1706a311eb0010048c4bc2dfe032b179f9665230
Luzzie-ux <estelarodrigoluz@gmail.com>
2026-07-22 23:14:02 +0100

    better optimization and testing

--- Libasm/ex4/ft_strlen.s
+++ Libasm/ex4/ft_strlen.s
@@ -7,14 +7,16 @@ global ft_strlen
 section .text
 
 ; long ft_strlen(const char *s)
-; Counts how many characters composes a
-; a null terminated string
+; Counts how many characters composes a null terminated string.
+; If a void ptr is passed, it will return 0
 ft_strlen:
-    xor rax, rax
+    xor rax, rax    ;zero outs rax
+    cmp rdi, 0      ; tests for a NULL value
+    je .return
     .loop:
-        cmp byte [rdi + rax], 0x0
-        je .return
-        inc rax
-        jmp .loop
+        cmp byte [rdi + rax], 0x0    ;looking for the end of the string
+        je .return                   ; if \0 then return
+        inc rax                      ; rax + 1 if not
+        jmp .loop                    ; go back to start of the loop
     .return:
-        ret
+        ret                          ; return point
--- Libasm/ex4/main.c
+++ Libasm/ex4/main.c
@@ -5,6 +5,7 @@ extern long ft_strlen(const char *s);
 int main(void)
 {
 	const char *str = "Hello, World!";
+	printf("%ld\n", ft_strlen(NULL));
 	printf("%ld\n", ft_strlen(str));
 	return (0);
 }
